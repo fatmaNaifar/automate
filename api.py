227ff6c6786d07dd5ae9973d982ac936416fece0
fatmaNaifar <fatma.naifar@enis.tn>
2024-08-11 17:19:50 +0100

    port...

--- api.py
+++ api.py
@@ -163,10 +163,9 @@ def email_form():
 def send_email():
     global email
     email = request.form.get('email')
-    print(request.form)
+    print(f"Form data received: {request.form}")
     if email:
         print(f"Received email: {email}")
-        threading.Thread(target=process_withings_data, args=(email,)).start()
         return 'Thank you, you can close this window'
     else:
         return jsonify({'status': 'error', 'message': 'Email not provided'}), 400
